8f0e0c1fd28a290e40910bf1af6779bcf71764e7
ArAs101 <armin.assadzadeh@stud.fh-campuswien.ac.at>
2024-05-03 17:44:41 +0200

    Rebased and cleaned up some code

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -16,35 +16,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("MainActivity", "onStart called.")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("MainActivity", "onResume called.")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("MainActivity", "onPause called.")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("MainActivity", "onStop called.")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("MainActivity", "onRestart called.")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("MainActivity", "onDestroy called.")
-    }
 }
-
